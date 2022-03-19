d4c4336ce4c130a7d8e4676526aaf3b0f9e5c02e
joshi233sj <joshi233sj@gmail.com>
2022-03-19 22:42:21 +0100

    Update ex_1b.py

--- 04/ex_1b.py
+++ 04/ex_1b.py
@@ -1,7 +1,9 @@
 from urllib import request
 from bs4 import BeautifulSoup as BS
 
-url = 'https://en.wikipedia.org/wiki/Lists_of_musicians' 
+painter_links = []
+
+url = 'https://en.wikipedia.org/wiki/List_of_hard_rock_musicians_(N%E2%80%93Z)'
 html = request.urlopen(url)
 bs = BS(html.read(), 'html.parser')
 
@@ -9,5 +11,7 @@ tags = bs.find_all('ul')[4].find_all('li')
 
 links = ['http://en.wikipedia.org' + tag.a['href'] for tag in tags]
 
-for link in links:
+painter_links.extend(links)
+
+for link in painter_links:
     print(link)
