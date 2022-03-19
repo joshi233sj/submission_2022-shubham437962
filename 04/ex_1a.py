97582dc100fd4d9289d73a6395882c2f18b7a924
joshi233sj <joshi233sj@gmail.com>
2022-03-19 22:37:54 +0100

    Create ex_1a.py
    task 1 a

---
+++ 04/ex_1a.py
@@ -0,0 +1,13 @@
+from urllib import request
+from bs4 import BeautifulSoup as BS
+
+url = 'https://en.wikipedia.org/wiki/Lists_of_musicians' 
+html = request.urlopen(url)
+bs = BS(html.read(), 'html.parser')
+
+tags = bs.find_all('ul')[19].find_all('li')
+
+links = ['http://en.wikipedia.org' + tag.a['href'] for tag in tags]
+
+for link in links:
+    print(link)
